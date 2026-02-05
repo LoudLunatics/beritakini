4c15f7fc70f0213cbe120da34e8f6493848a7c2c
LoudLunatics <nauvalazfa@proton.me>
2026-02-06 05:31:32 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useMemo } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import { getNews } from '../api/newsApi';
@@ -177,3 +178,4 @@ const Home = () => {
 };
 
 export default Home;
+
